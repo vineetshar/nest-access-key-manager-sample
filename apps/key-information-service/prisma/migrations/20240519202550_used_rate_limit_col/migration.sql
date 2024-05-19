82388f232fe99b643d12b486669957bd655421bd
Vineet <vn.sh@outlook.com>
2024-05-20 02:07:24 +0530

    Make token information service functional

---
+++ apps/key-information-service/prisma/migrations/20240519202550_used_rate_limit_col/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "UserAccessKey" ADD COLUMN     "usedlimit" INTEGER NOT NULL DEFAULT 0;
--- apps/key-information-service/src/token-information.service.ts
+++ apps/key-information-service/src/token-information.service.ts
@@ -1,19 +1,52 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { v4 as uuidv4 } from 'uuid';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class TokenInformationService {
   constructor(private prisma: PrismaService) {}
 
   async getTokenInformation(accessKey: string): Promise<string> {
-    const validateKey = await this.prisma.userAccessKey.findFirstOrThrow({
+    const sharedSecret = '1234';
+    const generatedKey = uuidv4();
+    const signature = crypto
+      .createHmac('sha256', sharedSecret)
+      .update(generatedKey)
+      .digest('hex');
+
+    const key = await this.prisma.userAccessKey.findFirstOrThrow({
       where: {
-        id: accessKey,
+        hash: '123',
       },
     });
 
-    console.log('accessKey', validateKey);
-    return 'Hello World!';
+    if (key.usedlimit === key.ratelimit) {
+      throw new BadRequestException('API Key limit exceeded');
+    }
+    if (!key.enabled) {
+      throw new BadRequestException('API Key not active');
+    }
+    if (!key.enabled) {
+      throw new BadRequestException('API Key not active');
+    }
+    if (key.expiry < new Date()) {
+      throw new BadRequestException('API Key has expired');
+    }
+
+    await this.prisma.userAccessKey.update({
+      where: {
+        id: key.id,
+      },
+      data: {
+        usedlimit: key.usedlimit + 1,
+      },
+    });
+    return { 'rate limit': key.ratelimit } as unknown as string;
   }
 }
